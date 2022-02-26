a02130d14f76bec5f6b3dbf4f5c27568895fe818
titikkoma88 <jie.koma88@gmail.com>
2022-02-26 17:08:47 +0700

    gorm get data table from databases

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"patungan/user"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,11 +11,30 @@ import (
 
 func main() {
 	dsn := "root:root@tcp(127.0.0.1:3306)/patungan?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to databases success!")
+
+	var users []user.User
+	length := len(users)
+	fmt.Println(length)
+
+	db.Find((&users))
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("===============")
+
+	}
+
 }
+
+
